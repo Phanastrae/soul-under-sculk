54cb71a38dce1f82988b779191d6b755841b3e7a
phanastrae <phanastrae@gmail.com>
2022-08-13 23:42:13 +0100

    changed all the basic config stuff, got the mod to actually launch

--- src/main/java/phanastrae/soul_under_sculk/mixin/TitleScreenMixin.java
+++ src/main/java/phanastrae/soul_under_sculk/mixin/TitleScreenMixin.java
@@ -1,16 +1,16 @@
-package com.example.example_mod.mixin;
+package phanastrae.soul_under_sculk.mixin;
 
-import com.example.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import phanastrae.soul_under_sculk.SoulUnderSculk;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		SoulUnderSculk.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
